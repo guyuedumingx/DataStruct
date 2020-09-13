2c107cc95bbcb94c5b2755b42e9c20613d5893ba
guyuedumingx <guyuedumingx@gmail.com>
2020-09-13 16:31:09 +0800

    update README.md

--- README.md
+++ README.md
@@ -1,12 +1,24 @@
 ## DataStruct
 
-### main.week_1  
-[**斐波那契数列**](src/main/java/week_1/Fabonacci.java)
+### Week_1  
+[**斐波那契数列**](src/main/java/week_1/Fabonacci.java)  
+
 [**递归**](src/main/java/week_1/Factorial.java)  
+
 [**百钱百鸡问题**](src/main/java/week_1/ChickAndHen.java)  
-[**冒泡排序与快速排序**](src/main/java/week_1/BubbleSort.java)  
+![百钱百鸡函数拟合](res/image/HensAndCock.png)  
+
+[**冒泡排序与快速排序**](src/main/java/week_1/BubbleSort.java)    
+*冒泡排序*  
+![冒泡排序动图](res/image/BubbleSort.gif)  
+*快速排序*  
+![快速排序动图](res/image/QuickSort.gif)  
 
 ### Contributer  
 
 guyuedumingx  
+
+### Web  
+
+[十大经典排序动图](https://www.cnblogs.com/onepixel/articles/7674659.html)  
 
--- src/main/java/week_1/ChickAndHen.java
+++ src/main/java/week_1/ChickAndHen.java
@@ -18,16 +18,16 @@ public class ChickAndHen {
     static float chick_pride = 1/3;
 
     public static void main(String[] args) {
-//        int[] x = setArr();
-//        double[] y = new double[100];
-//        for (int i=0; i<x.length;i++) {
-//            long res = calculate(x[i]);
-//            y[i] = res*Math.pow(10,-6);
-//        }
-//        prt(x);
-//        prt(y);
-        long calculate = calculate(1000);
-        prt(calculate);
+        int[] x = setArr();
+        double[] y = new double[100];
+        for (int i=0; i<x.length;i++) {
+            long res = calculate(x[i]);
+            y[i] = res;
+        }
+        prt(x);
+        prt(y);
+//        long calculate = calculate(4000);
+//        prt(calculate);
     }
 
     /**
@@ -39,7 +39,7 @@ public class ChickAndHen {
         int[] arr = new int[number];
         for (int i=0; i<number; i++) {
             arr[i] = first;
-            first += 100;
+            first += number;
         }
         return arr;
     }
@@ -64,8 +64,8 @@ public class ChickAndHen {
                 //小鸡数
                 int chick_number = sum - cock_number - hem_number;
                 //小鸡数量必须是3的倍数,各种鸡的数量和价格乘积为钱数
-                if(chick_number%3==0 && (cock_number*cock_pride+hem_number* hen_pride +
-                chick_number*chick_pride)==sum){}
+                if(chick_number%3==0 && ((cock_number*cock_pride+hem_number* hen_pride +
+                chick_number*chick_pride)==sum)){}
             }
         }
         long end = System.nanoTime();
@@ -89,7 +89,7 @@ public class ChickAndHen {
      */
     public static void prt(double[] arr) {
         for (double i: arr) {
-            System.out.print(i+",");
+            System.out.print(i*Math.pow(10,-6)+",");
         }
         System.out.println();
     }
