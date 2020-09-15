427f88c4c358ebd760ff17a6b54c56425ebf5bb7
guyuedumingx <guyuedumingx@gmail.com>
2020-09-16 00:25:49 +0800

    add Multiply2

--- src/main/java/week_2/Multiply2.java
+++ src/main/java/week_2/Multiply2.java
@@ -1,4 +1,19 @@
 package week_2;
 
+import java.util.Arrays;
+
 public class Multiply2 {
+    public static void main(String[] args) {
+        int[] num1 = Multiply.getBitArray("234");
+        int[] num2 = Multiply.getBitArray("12358");
+        int[] sum = sum = Arrays.copyOf(num1,num1.length+num2.length);
+        for (int i=0; i<num1.length; i++) {
+            for (int n=0; n<num2.length; n++) {
+                sum[i+n] += sum[i] * num2[n];
+                sum[i+1+n] += sum[i+n] / 10;
+                sum[i+n] = sum[i+n] % 10;
+            }
+        }
+        Multiply.parseArray(sum);
+    }
 }
