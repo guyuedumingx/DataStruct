b30abfe229839f05f534e9a715e19682e2968238
guyuedumingx <guyuedumingx@gmail.com>
2020-09-21 13:28:14 +0800

    update wangwei OneDimensional and TwoDimensional

--- src/main/java/week_2/wangwei/marquee/OneDimensional.java
+++ src/main/java/week_2/wangwei/marquee/OneDimensional.java
@@ -33,11 +33,18 @@ public class OneDimensional {
      * @return 返回的数组，用于验证
      */
     private static int[] run(int[] arr, int step) {
+        //创建一个数组用于保存跑完一次的数据
         int[] res = new int[arr.length];
+        //原数组下标指针
         int i = 0;
+        //res数组下标指针
         int j = 0;
+        //保存原数组的第一位数值，因为该数组的所有值都不相同，所以当res[0]==first的时候
+        //跑马灯就跑完了
         int first = arr[0];
+        //i==arr.length表示arr完整的遍历了一次,防止出现[1,0,0,0,5,6,7,8,9]这种情况
         while (!(res[0]==first && i==arr.length)) {
+            //跑完一次后把res数组给arr，再新建一个res对象进行新一轮的遍历
             if(i==arr.length) {
                 prt(res);
                 arr = res;
@@ -46,6 +53,7 @@ public class OneDimensional {
                 j = 0;
             }
 
+            //这个表达式要是看不懂就单步调试来看
             j = (i+step)%arr.length;
             res[j] = arr[i++];
         }
