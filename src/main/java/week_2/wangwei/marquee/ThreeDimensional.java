fe70dd1011411ada96886d743f1fdb59dc27399e
guyuedumingx <guyuedumingx@gmail.com>
2020-09-22 01:43:24 +0800

    add Twodimensional

--- src/main/java/week_2/wangwei/marquee/ThreeDimensional.java
+++ src/main/java/week_2/wangwei/marquee/ThreeDimensional.java
@@ -3,108 +3,89 @@ package week_2.wangwei.marquee;
 import java.io.IOException;
 
 public class ThreeDimensional {
-
-    private static int step = 2;
-    private static int origin_length = 4;
-    private static int length = origin_length;
-    private static int[][][] arr = createArray(origin_length);
-    private static int[][][] res = new int[origin_length][origin_length][origin_length];
-    private static int[] diagonalNumbers = new int[origin_length/2];
+    static int length = 3;
+    static int[][][] arr = createArray(length);
+    static int[][][] res = new int[length][length][length];
+    static int step = 2;
+    static int x = 0;
+    static int y = 0;
+    static int z = 0;
+    static char c = 'x';
 
     public static void main(String[] args) throws IOException {
-        DiagonalsNumbers();
-        do {
-            prt(arr);
-            for(int i=0; i<origin_length/2; i++) {
-                goRight(i, i);
-                goDown(i, length - 1);
-                goLeft(length - 1, length - 1);
-                goUp(length - 1, i);
-                length -= 1;
-            }
-            length = origin_length;
-            arr = res;
-            res = new int[origin_length][origin_length][origin_length];
-        }while (!isEquals(arr));
-        prt(arr);
+        int[][] twoDimensionalArray = getTwoDimensionalArray();
+        int[][] run = TwoDimensional.run(twoDimensionalArray);
+        saveValue(run);
+        System.out.println("length");
     }
 
-    private static void DiagonalsNumbers() {
-        for(int i=0; i<origin_length/2; i++) {
-            diagonalNumbers[i] = arr[i][i];
+
+
+    public static int[][] getTwoDimensionalArray() {
+        int[][] temp = new int[length][length];
+        switch (c) {
+            case 'x':
+                for(int i=0; i<length; i++) {
+                    for(int j=0; j<length; j++) {
+                        temp[i][j] = arr[x][i][j];
+                    }
+                }
+                break;
+            case 'y':
+                for(int i=0; i<length; i++){
+                    for(int j=0; j<length; j++) {
+                        temp[i][j] = arr[i][y][j];
+                    }
+                }
+                break;
+            case 'z':
+                for(int i=0; i<length; i++) {
+                    for(int j=0; j<length; j++) {
+                        temp[i][j] = arr[i][j][z];
+                    }
+                }
+                break;
         }
+        return temp;
     }
 
-    private static boolean isEquals(int[][][] arr) {
-        boolean flag = true;
-        for(int i=0; i<origin_length/2 && flag; i++) {
-            flag = (diagonalNumbers[i]==arr[i][i]);
+    public static void saveValue(int[][] temp) {
+        switch (c) {
+            case 'x':
+                for(int i=0; i<length; i++) {
+                    for(int j=0; j<length; j++) {
+                        arr[x][i][j] = temp[i][j];
+                    }
+                }
+                break;
+            case 'y':
+                for(int i=0; i<length; i++){
+                    for(int j=0; j<length; j++) {
+                        arr[i][y][j] = temp[i][j];
+                    }
+                }
+                break;
+            case 'z':
+                for(int i=0; i<length; i++) {
+                    for(int j=0; j<length; j++) {
+                        arr[i][j][z] = temp[i][j];
+                    }
+                }
+                break;
         }
-        return flag;
+
     }
 
-    private static void goRight(int i, int j) {
-        while (j+step<length) {
-            res[i][j+step] = arr[i][j++];
-        }
-        while (j<length-1) {
-            int k = (j+step)%(length-1);
-            res[i+k][length-1] = arr[i][j++];
-        }
-    }
-    private static void goDown(int i, int j) {
-        while (i+step<length) {
-            res[i+step][j] = arr[i++][j];
-        }
-        while (i<length-1) {
-            int k = (i+step)%(length-1);
-            res[length-1][length-k-1] = arr[i++][j];
-        }
-    }
-    private static void goLeft(int i, int j) {
-        //最小脚标
-        int min = origin_length - length;
-        while (j-step>=min) {
-            res[i][j-step] = arr[i][j--];
-        }
-        while (j>min) {
-            int k = (origin_length-1-j+step)%(length-1);
-            res[length-k-1][min] = arr[i][j--];
-        }
-    }
-
-    private static void goUp(int i, int j) {
-        int min = origin_length - length;
-        while (i-step>=min) {
-            res[i-step][j] = arr[i--][j];
-        }
-        while (i>min) {
-            int k = (origin_length-i+step)%(length);
-            res[min][k+j] = arr[i--][j];
-        }
-    }
-
-    public static int[][][] createArray(int length) {
-        int[][][] arr =  new int[length][length][length];
-        int n = 1;
-        for(int i=0;i<length;i++) {
-            for(int j=0; j<length; j++) {
-                for(int k=0;k<length; k++) {
-                    arr[i][j][k] = n++;
+    public static int[][][] createArray(int x) {
+        int[][][] arr = new int[x][x][x];
+        int z = 0;
+        for(int i=0; i<x; i++) {
+            for(int j=0; j<x; j++) {
+                for(int k=0; k<x; k++) {
+                   arr[i][j][k] = ++z;
                 }
             }
         }
         return arr;
-    }
-
-    public static void prt(int[][][] arr, int zhou) {
-        for(int i=0; i<arr.length; i++) {
-            System.out.print("[");
-            for (int j=0; j<arr[i].length; j++) {
-                System.out.print(arr[zhou][i][j]+"\t");
-            }
-            System.out.println("]");
-        }
-        System.out.println();
     }
 }
