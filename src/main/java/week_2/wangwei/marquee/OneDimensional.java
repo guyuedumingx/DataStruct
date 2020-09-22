177a10f006809cae868357bbe5b94b39615c1112
guyuedumingx <guyuedumingx@gmail.com>
2020-09-22 09:50:34 +0800

    update OneDimensional

--- src/main/java/week_2/wangwei/marquee/OneDimensional.java
+++ src/main/java/week_2/wangwei/marquee/OneDimensional.java
@@ -7,9 +7,11 @@ package week_2.wangwei.marquee;
  * @author wangwei
  */
 public class OneDimensional {
+    static int[] stepArr = new int[]{3,5,8,13};
+
     public static void main(String[] args) {
-        int[] arr = createArray(200);
-        int[] run = run(arr, 3);
+        int[] arr = createArray(101);
+        int[] run = run(arr);
         prt(run);
     }
 
@@ -29,16 +31,16 @@ public class OneDimensional {
     /**
      * 跑马灯的核心方法
      * @param arr 传入数组
-     * @param step 每步移动的步长
      * @return 返回的数组，用于验证
      */
-    private static int[] run(int[] arr, int step) {
+    private static int[] run(int[] arr) {
         //创建一个数组用于保存跑完一次的数据
         int[] res = new int[arr.length];
         //原数组下标指针
         int i = 0;
         //res数组下标指针
         int j = 0;
+        int k = 0;
         //保存原数组的第一位数值，因为该数组的所有值都不相同，所以当res[0]==first的时候
         //跑马灯就跑完了
         int first = arr[0];
@@ -46,15 +48,15 @@ public class OneDimensional {
         while (!(res[0]==first && i==arr.length)) {
             //跑完一次后把res数组给arr，再新建一个res对象进行新一轮的遍历
             if(i==arr.length) {
-                prt(res);
                 arr = res;
                 res = new int[arr.length];
                 i = 0;
                 j = 0;
+                k++;
+                k = k<stepArr.length ? k : 0;
             }
-
             //这个表达式要是看不懂就单步调试来看
-            j = (i+step)%arr.length;
+            j = (i+stepArr[k])%arr.length;
             res[j] = arr[i++];
         }
         return res;
