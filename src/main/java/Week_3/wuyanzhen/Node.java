d91ebd286f578e8220c4ed923f81bbb18e817b18
Florence_y <2213576511@qq.com>
2020-10-03 14:53:21 +0800

    吴彦臻完成weekfour

---
+++ src/main/java/Week_3/wuyanzhen/Node.java
@@ -0,0 +1,12 @@
+package Week_3.wuyanzhen;
+
+
+/**
+ * @author Florence
+ */
+public class Node<T> {
+    public T data;
+    public Node<T> next=null;
+    public Node(T data){ this.data=data; }
+    public Node(){ }
+}
