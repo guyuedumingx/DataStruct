091c2164cf1ab3838078fa1f7d044da12844ec0d
QYIF <499600328@qq.com>
2020-11-12 00:58:54 +0800

    update w7

--- src/main/java/util/dqy/QyTree.java
+++ src/main/java/util/dqy/QyTree.java
@@ -23,10 +23,24 @@ public class QyTree<T> {
         root.val = src.next();
         QyQueue<Node> q = new QyQueue<Node>();
         q.push(root);
+        //当前队列不为空时
         while (!q.isEmpty()) {
+            //获取结点
             Node cur = q.getFront();
             q.pop();
-
+            //创建两个结点分别为左节点和右节点
+            Node ln = new Node(src.next());
+            Node rn = new Node(src.next());
+            //“#”代表空
+            //判断当前点是否为空，为空则连入结点中，并继续处理后续的点
+            if (!ln.val.equals("#")) {
+                cur.left = ln;
+                q.push(ln);
+            }
+            if (!rn.val.equals("#")) {
+                cur.right = rn;
+                q.push(rn);
+            }
         }
         return newTree;
     }
