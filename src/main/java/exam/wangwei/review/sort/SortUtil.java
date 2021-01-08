8c86c75f631a3f29e0778ef194a819e6f6f7800c
guyuedumingx <guyuedumingx@gmail.com>
2021-01-08 09:36:16 +0800

    添加排序的工具包

---
+++ src/main/java/exam/wangwei/review/sort/SortUtil.java
@@ -0,0 +1,14 @@
+package exam.wangwei.review.sort;
+
+/**
+ * 工具包
+ * @author yohoyes
+ */
+public class SortUtil {
+
+    public static void prt(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i+"\t");
+        }
+    }
+}
---
+++ src/main/java/exam/wangwei/review/sort/change/QuickSort.java
@@ -0,0 +1,12 @@
+package exam.wangwei.review.sort.change;
+
+/**
+ * 快速排序
+ * @author yohoyes
+ */
+public class QuickSort {
+
+    public static void sort(int[] arr) {
+
+    }
+}
